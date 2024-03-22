6ae55670d72f4c733277df2479c8bf7db375db3d
muaz-405 <muazjunejo786@gmail.com>
2024-03-23 00:38:01 +0500

    update

---
+++ dom.js
@@ -0,0 +1,7 @@
+function update(){
+    console.log(" Updating results");
+
+var input = document.getElementById('myinput');
+var output = document.getElementById('output');
+output.innerHTML = input.value;
+}
---
+++ event.js
@@ -0,0 +1,15 @@
+function update(){
+    console.log("event is fired");
+}
+//i want to create variable
+function update(){
+    console.log(x+y);
+}
+var x = 10;
+var y = 20;
+
+function sum(a,b){
+
+    return a+b;
+    
+}
